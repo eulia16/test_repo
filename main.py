2f751652b5a79eb25e9c7f00410c319da9d57a6a
EUli16 <ethan.uliano@gmail.com>
2022-12-18 15:17:21 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+
+#from flask import Flask, requests
+
+#imports
+import json
+import random
+
+import requests
+
+#this sends a request and returns all JSON returned from the API
+def send_request(num_questions=10):
+    response_API = requests.get('https://the-trivia-api.com/api/questions?limit=' + str(num_questions))
+    data = response_API.text
+    parse_json = json.loads(data)
+    return parse_json
+
+#add fucntions to return specific parts of the json
+#function will take in question id(or maybe number), and
+#then return the question for example
+
+
+
+def random_individual_question(parse_json):
+    #returns a random question
+    random_value = (len(parse_json)-1)
+    return parse_json[random.randint(0, random_value)]
+
+def selected_individual_question(parse_json, selected_number):
+    if selected_number > (len(parse_json)-1):
+        return -1
+    else:
+        return parse_json[selected_number]
+
+
+
+
+if __name__ == '__main__':
+    temp_value = send_request(5)
+    print(temp_value)
+    print(random_individual_question(temp_value))
